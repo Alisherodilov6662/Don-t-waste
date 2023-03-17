252da142faaa53d13f9acf03a6b4fff0a0048d10
Alisher <alisherodilov482@gmail.com>
2023-03-17 05:34:42 -0700

    for package and class

---
+++ src/main/java/com/example/DoNotWasteApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoNotWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoNotWasteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/controller/AttachController.java
@@ -0,0 +1,8 @@
+package com.example.controller;
+
+/**
+ * Author: Alisher Odilov
+ * Date: 17.03.2023
+ */
+public class AttachController {
+}
