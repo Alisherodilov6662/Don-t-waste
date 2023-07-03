bde4fb2607014bbbb83cf04ef521dc00a0ed3d8a
Alisher <alisherodilov482@gmail.com>
2023-07-03 00:04:59 -0700

    --

---
+++ src/main/java/com/example/entity/advert/AdvertSectionEntity.java
@@ -0,0 +1,39 @@
+package com.example.entity.advert;
+
+import com.example.entity.AttachEntity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * Author: Alisher Odilov
+ * Date: 18.06.2023
+ */
+@Entity
+@Table(name = "advert_section_entity")
+public class AdvertSectionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "photo_id")
+    private String photoId;
+
+    @OneToOne
+    @JoinColumn(name = "photo_id", insertable = false, updatable = false)
+    private AttachEntity photo;
+
+    @Column(name = "visible")
+    private Boolean visible;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+}
