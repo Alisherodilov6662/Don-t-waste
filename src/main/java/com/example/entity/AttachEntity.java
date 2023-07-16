37daa7a2d587fd7eabd00316d8d60b56cfb077cf
Alisher <alisherodilov482@gmail.com>
2023-07-16 04:58:13 -0700

    AttachEntity

--- src/main/java/com/example/entity/AttachEntity.java
+++ src/main/java/com/example/entity/AttachEntity.java
@@ -1,12 +1,16 @@
 package com.example.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 /**
  * Author: Alisher Odilov
  * Date: 17.03.2023
  */
+@Getter
+@Setter
 @Entity
 @Table(name = "attach")
 public class AttachEntity {
@@ -14,5 +18,17 @@ public class AttachEntity {
     @Id
     @GenericGenerator(name = "attach_uuid", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
+
+    @Column
+    private String originalName;
+
+    @Column
+    private Long size;
+    
+    @Column
+    private String type;
+
+    @Column
+    private String path;
 
 }
