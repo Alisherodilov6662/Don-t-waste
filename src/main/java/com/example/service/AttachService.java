07363fb6a9a2bba1877f173d28b878bfb32a2c4f
Alisher <alisherodilov482@gmail.com>
2023-07-16 07:27:29 -0700

    AttachService is added

---
+++ src/main/java/com/example/service/AttachService.java
@@ -0,0 +1,25 @@
+package com.example.service;
+
+import com.example.dto.AttachResponseDTO;
+import com.example.repository.AttachmentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Author: Alisher Odilov
+ * Date: 16.07.2023
+ */
+@Service
+@AllArgsConstructor
+public class AttachService {
+
+    private AttachmentRepository attachmentRepository;
+
+    public AttachResponseDTO uploadFile(MultipartFile file) {
+    }
+
+    public Resource downloadFile(String id) {
+    }
+}
